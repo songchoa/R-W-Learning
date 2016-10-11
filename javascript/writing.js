6f3be96fcff4ee203aa609482fb986a1fe44a698
Val Chapman <vchapman@iastate.edu>
2016-10-11 11:43:52 -0500

    Game with end game function

--- javascript/writing.js
+++ javascript/writing.js
@@ -5,16 +5,25 @@ var platforms;
 var wordList = ["cat", "dog", "bat","rat","bog","hog","yup","fun","run","ton", "zoo"];
 var wordType;
 var timer;
-var cutTile;
+var curTile;
+var wordTile;
+var playerIMG =  new Image();
+var tileIMG = new Image();
+var gameInt
+var score = 0;
 var Write = {
 
 	run : function(){
 		var ctx = document.getElementById("myCanvas");
 		canvas = document.getElementById("myCanvas"),
  	 	this.context = ctx.getContext("2d");
- 		this.interval = setInterval(updateGameArea, 20);
+ 		gameInt = setInterval(updateGameArea, 20);
  		platforms = [];
  		wordType = new createWord(0,0,0,0);
+ 		playerIMG.src = "../res/smallHat.jpg";
+ 		playerIMG.width = 200;
+ 		playerIMG.height = 200;
+ 		tileIMG.src = "../res/platform.png";
  		var topW;
  		var topH;
  		for(var x = 0; x < 5; x++){
@@ -76,7 +85,15 @@ function playerAnswer(){
 	if(answer == wordType.text){
 		myGamePiece.x = wordType.x + 10;
 		myGamePiece.y = wordType.y- 70;
-		platforms.splice(curTile,1);
+		console.log("curTile = " + curTile);
+		console.log("wordTile = " + wordTile);
+		if(curTile != undefined){
+			platforms.splice(curTile,1);
+			curTile = wordTile;
+		}else{
+			curTile = wordTile
+		}
+		score++;
 		placeWord();
 		document.getElementById("userIn").text = "";
 		//timer = setInterval(,1000);
@@ -94,21 +111,33 @@ function PC(){
 	this.y = 200;
 	ctx =  Write.context;
 	ctx.fillStyle = "red";
-	ctx.fillRect(this.x, this.y, this.width, this.height);
+
+	//ctx.fillRect(this.x, this.y, this.width, this.height);
 	this.update = function(){
         ctx = Write.context;
         ctx.fillStyle = "red";
+        //ctx.drawImage(playerIMG,this.width,this.height);
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 }
 
 function placeWord(){
+	if(platforms.length < 2){
+		alert("Game Over Start New Game");
+		Write.clear();
+		clearInterval(gameInt);
+		return;
+	}
+
 	rand = Math.floor((Math.random() * 10) + 1);
 	var step;
 	//console.log(rand);
 	wordType.text = wordList[rand];
-	curTile = Math.floor((Math.random() * platforms.length) + 1);
-	step = platforms[curTile];
+	wordTile = Math.floor((Math.random() * platforms.length) + 1);
+	while(wordTile == curTile){
+		wordTile = Math.floor((Math.random() * platforms.length) + 1);
+	}
+	step = platforms[wordTile];
 	wordType.x = step.x ;
 	wordType.y = step.y + 80;
 }
