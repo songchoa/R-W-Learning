2296dd512c5f70cbb15e782054a6b7cd43b94939
Val Chapman <vchapman@iastate.edu>
2016-10-11 23:42:36 -0500

    Final Product with small bugs

--- javascript/writing.js
+++ javascript/writing.js
@@ -4,26 +4,43 @@ var canvas;
 var platforms;
 var wordList = ["cat", "dog", "bat","rat","bog","hog","yup","fun","run","ton", "zoo"];
 var wordType;
+var sBoard;
 var timer;
 var curTile;
 var wordTile;
 var playerIMG =  new Image();
 var tileIMG = new Image();
-var gameInt
+var tileTime;
+var backIMG = new Image();
 var score = 0;
+var tileLife;
+var gameInt
 var Write = {
 
 	run : function(){
 		var ctx = document.getElementById("myCanvas");
 		canvas = document.getElementById("myCanvas"),
  	 	this.context = ctx.getContext("2d");
- 		gameInt = setInterval(updateGameArea, 20);
+ 	 	
+ 	 	if(gameInt === undefined){
+ 	 		gameInt = setInterval(updateGameArea, 20);
+ 			tileLife = setInterval(reduceTime,2000);
+ 		}
+ 		else{
+ 	 		clearInterval(gameInt);
+ 	 		clearInterval(tileLife);
+ 	 		gameInt = setInterval(updateGameArea, 20);
+ 			tileLife = setInterval(reduceTime,2000);
+ 	 	}
  		platforms = [];
  		wordType = new createWord(0,0,0,0);
  		playerIMG.src = "../res/smallHat.jpg";
- 		playerIMG.width = 200;
- 		playerIMG.height = 200;
  		tileIMG.src = "../res/platform.png";
+ 		backIMG.src = "../res/backGround.png";
+ 		sBoard = new scoreBoard();
+ 		score = 0;
+ 		tileTime = 20;
+ 		curTile = 3;
  		var topW;
  		var topH;
  		for(var x = 0; x < 5; x++){
@@ -68,27 +85,56 @@ function createPlat(width, height, x, y){
 	this.height = height;
 	this.x = x;
 	this.y = y;
-	var time = 20;
+	this.time = 20;
 	ctx = canvas.getContext("2d");
 	ctx.fillStyle = "blue";
 	ctx.fillRect(this.x, this.y, this.width,this.height);
 	this.update = function(){
 		ctx = Write.context;
-		ctx.fillStyle = "blue";
-		ctx.fillRect(this.x, this.y,this.width,this.height);
+		ctx.drawImage(tileIMG,this.x,this.y);
 	}
 
 }
 
+function scoreBoard(){
+	this.x = 465;
+	this.y = 15;
+	this.update = function(){
+		ctx = Write.context;
+		ctx.fillStyle = "tan";
+		ctx.fillRect(this.x,this.y,130,300);
+		ctx.fillStyle = "black";
+		ctx.font = "40px Arial";
+		ctx.fillText("Score: ", this.x + 5,this.y + 45);
+		ctx.fillText(score,this.x + 5, this.y + 90);
+		ctx.fillText("Time: ", this.x + 5,this.y + 130);
+		if(tileTime > 10){
+			ctx.fillStyle = "green";
+			ctx.fillText(tileTime,this.x + 5, this.y + 170);
+		}
+		else if(tileTime > 4){
+			ctx.fillStyle = "yellow";
+			ctx.fillText(tileTime,this.x + 5, this.y + 170);
+		}
+		else{
+			ctx.fillStyle = "red";
+			ctx.fillText(tileTime,this.x + 5, this.y + 170);;
+		}
+	}
+}
+
+
 function playerAnswer(){
 	var answer = document.getElementById("userIn").value;
 	if(answer == wordType.text){
-		myGamePiece.x = wordType.x + 10;
-		myGamePiece.y = wordType.y- 70;
+		myGamePiece.x = wordType.x - 5;
+		myGamePiece.y = wordType.y - 40;
 		console.log("curTile = " + curTile);
 		console.log("wordTile = " + wordTile);
 		if(curTile != undefined){
-			platforms.splice(curTile,1);
+			if(platforms[curTile].time < 4){
+				platforms.splice(curTile,1);
+			}
 			curTile = wordTile;
 		}else{
 			curTile = wordTile
@@ -101,7 +147,27 @@ function playerAnswer(){
 }
 
 function reduceTime(){
-
+	if(curTile != undefined){
+		platforms[curTile].time--;
+		tileTime = platforms[curTile].time;
+		if(platforms[curTile].time <= 0){
+			clearInterval(gameInt);
+			clearInterval(tileLife);
+			ctx = Write.context;
+			Write.clear();
+			ctx.drawImage(backIMG, -20, -20);
+			ctx.fillStyle = "tan";
+			ctx.fillRect(40,40,370,250);
+			ctx.fillStyle = "black";
+			ctx.font = "45px Arial";
+			ctx.fillText("Game Over!", 55,90);
+			ctx.font = "35px Arial";
+			ctx.fillText("Your score was: ", 55, 160);
+			ctx.fillText(score + "!",55, 200);
+			ctx.fillText("Press start new game", 55,240);
+			ctx.fillText("to beat your score!", 55,280);
+		}
+	}
 }
 
 function PC(){
@@ -115,9 +181,9 @@ function PC(){
 	//ctx.fillRect(this.x, this.y, this.width, this.height);
 	this.update = function(){
         ctx = Write.context;
-        ctx.fillStyle = "red";
-        //ctx.drawImage(playerIMG,this.width,this.height);
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        //ctx.fillStyle = "red";
+        ctx.drawImage(playerIMG,this.x, this.y);
+        //ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 }
 
@@ -133,19 +199,22 @@ function placeWord(){
 	var step;
 	//console.log(rand);
 	wordType.text = wordList[rand];
-	wordTile = Math.floor((Math.random() * platforms.length) + 1);
+	wordTile = Math.floor((Math.random() * 10) + 1);
 	while(wordTile == curTile){
 		wordTile = Math.floor((Math.random() * platforms.length) + 1);
 	}
 	step = platforms[wordTile];
-	wordType.x = step.x ;
-	wordType.y = step.y + 80;
+	wordType.x = step.x + 15 ;
+	wordType.y = step.y + 55;
 }
 function updateGameArea(){
 	Write.clear();
+	ctx = Write.context;
+		ctx.drawImage(backIMG, -20, -20);
 	for(var x = 0; x < platforms.length; x++){
  				platforms[x].update();
  	}
+ 	sBoard.update();
  	myGamePiece.update();
  	wordType.update();
 }
